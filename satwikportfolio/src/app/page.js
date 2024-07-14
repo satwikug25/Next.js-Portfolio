1708ef1e82c02e0a68c8e9e10ae0a40f65a38acb
Sai Shiva Satwik Mallajosyula <satwikug@gmail.com>
2024-07-14 03:41:51 -0700

    typescript added

--- satwikportfolio/src/app/page.js
+++ satwikportfolio/src/app/page.js
@@ -21,10 +21,10 @@ export default function Home() {
           >
             <h1 className="text-4xl font-bold text-white">Satwik Mallajosyula</h1>
             <p className="text-lg text-white mt-4">
-              I&apos;m a 
+              I&apos;m a  
               <span className="text-yellow-300">
                 <Typewriter
-                  words={['Computer Science student', 'React Developer', 'Full Stack Developer', 'Software Engineer Intern']}
+                  words={[' Computer Science Student @ ASU', ' React Developer', ' Full Stack Developer', ' Software Engineer Intern @ Axio AI']}
                   loop={0}
                   cursor
                   cursorStyle='_'
